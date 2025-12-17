3a164fab56dd296cde24d0b92176a27580986ed1
che2n3jigw <che2n3jigw@gmail.com>
2025-12-17 22:47:27 +0800

    chore(project): init

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        gradlePluginPortal()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+rootProject.name = "Subsonic API"
+include(":app")
+ 
