b9048bbbe9ae88f8b0cccb3f350ba5c4b2395c5a
che2n3jigw <che2n3jigw@gmail.com>
2025-12-19 23:49:14 +0800

    feat(subsonic): 定义基础数据结构

---
+++ lib_subsonic_api/src/main/java/com/che2n3jigw/android/libs/subsonicapi/bean/response/ISubsonicResponse.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 che2n3jigw.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+// 作者: che2n3jigw
+// 邮箱: che2n3jigw@163.com
+// 博客: che2n3jigw.github.io
+// 创建时间： 12/19/25
+package com.che2n3jigw.android.libs.subsonicapi.bean.response
+
+/**
+ * Subsonic接口基础数据类型
+ */
+interface ISubsonicResponse {
+    val status: String?
+    val version: String?
+}
