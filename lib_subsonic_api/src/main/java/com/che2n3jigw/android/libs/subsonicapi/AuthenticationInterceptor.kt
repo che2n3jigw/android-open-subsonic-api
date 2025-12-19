7b30ee922bc9974ba3fa4d61c87fa8ddfb49318f
che2n3jigw <che2n3jigw@gmail.com>
2025-12-19 23:48:13 +0800

    feat(subsonic): 创建授权认证参数拦截器

---
+++ lib_subsonic_api/src/main/java/com/che2n3jigw/android/libs/subsonicapi/AuthenticationInterceptor.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2025 che2n3jigw.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+// 作者: che2n3jigw
+// 邮箱: che2n3jigw@163.com
+// 博客: che2n3jigw.github.io
+// 创建时间： 12/18/25
+package com.che2n3jigw.android.libs.subsonicapi
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+/**
+ * 认证拦截器
+ */
+class AuthenticationInterceptor(
+    private val username: String,
+    private val password: String
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        // 原始请求
+        val originalRequest = chain.request()
+        // 原始请求地址
+        val originalHttpUrl = originalRequest.url
+        // 原始地址追加参数
+        val url = originalHttpUrl.newBuilder()
+            // 用户名
+            .addQueryParameter("u", username)
+            // 密码
+            .addQueryParameter("p", password)
+            // 协议版本
+            .addQueryParameter("v", "1.16.1")
+            // 客户端标识
+            .addQueryParameter("c", "android-client")
+            // 响应数据格式 json
+            .addQueryParameter("f", "json")
+            .build()
+
+        val request = originalRequest.newBuilder()
+            .url(url)
+            .build()
+
+        return chain.proceed(request)
+    }
+}
