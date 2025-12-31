cf0727de67d4833edd2da6e04e5a227add247052
che2n3jigw <che2n3jigw@gmail.com>
2025-12-31 16:42:16 +0800

    feat(playlists): 添加Playlists API接口与数据模型
    为Open Subsonic API添加了完整的Playlists API功能。此提交包括：
    - 新增`PlaylistsService`接口，定义了`createPlaylist`、`deletePlaylist`、`getPlaylist`、`getPlaylists`和`updatePlaylist`等端点。
    - 引入了用于处理播放列表相关响应的数据模型，包括`Playlist`、`Playlists`、`PlaylistWithSongs`以及各个接口专用的响应类。

---
+++ lib_open_subsonic_api/src/main/java/com/che2n3jigw/android/libs/opensubsonicapi/repository/PlaylistsService.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2025 che2n3jigw.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+// 作者: che2n3jigw
+// 邮箱: che2n3jigw@163.com
+// 博客: che2n3jigw.github.io
+// 创建时间： 12/31/25
+package com.che2n3jigw.android.libs.opensubsonicapi.repository
+
+import com.che2n3jigw.android.libs.opensubsonicapi.response.BaseResponse
+import com.che2n3jigw.android.libs.opensubsonicapi.response.common.SubsonicResponse
+import com.che2n3jigw.android.libs.opensubsonicapi.response.playlists.CreatePlaylistResponse
+import com.che2n3jigw.android.libs.opensubsonicapi.response.playlists.GetPlaylistResponse
+import com.che2n3jigw.android.libs.opensubsonicapi.response.playlists.GetPlaylistsResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * open subsonic playlists api
+ */
+interface PlaylistsService {
+    /**
+     * Creates (or updates) a playlist.
+     */
+    @GET("/rest/createPlaylist")
+    suspend fun createPlaylist(
+        @Query("playlistId") playlistId: String,
+        @Query("name") name: String,
+        @Query("songId") songId: List<String>
+    ): BaseResponse<CreatePlaylistResponse>
+
+    /**
+     * Deletes a saved playlist.
+     */
+    @GET("/rest/deletePlaylist")
+    suspend fun deletePlaylist(
+        @Query("id") id: String
+    ): BaseResponse<SubsonicResponse>
+
+    /**
+     * Returns a listing of files in a saved playlist.
+     */
+    @GET("/rest/getPlaylist")
+    suspend fun getPlaylist(
+        @Query("id") id: String
+    ): BaseResponse<GetPlaylistResponse>
+
+    /**
+     * Returns all playlists a user is allowed to play.
+     */
+    @GET("/rest/getPlaylists")
+    suspend fun getPlaylists(
+        @Query("username") username: String
+    ): BaseResponse<GetPlaylistsResponse>
+
+    /**
+     * Updates a playlist. Only the owner of a playlist is allowed to update it.
+     */
+    @GET("/rest/updatePlaylist")
+    suspend fun updatePlaylist(
+        @Query("playlistId") playlistId: String,
+        @Query("name") name: String,
+        @Query("comment") comment: String,
+        @Query("public") public: Boolean,
+        @Query("songIdToAdd") songIdToAdd: List<String>,
+        @Query("songIndexToRemove") songIndexToRemove: List<Int>
+    ): BaseResponse<SubsonicResponse>
+}
---
+++ lib_open_subsonic_api/src/main/java/com/che2n3jigw/android/libs/opensubsonicapi/response/playlists/PlaylistWithSongs.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2025 che2n3jigw.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+// 作者: che2n3jigw
+// 邮箱: che2n3jigw@163.com
+// 博客: che2n3jigw.github.io
+// 创建时间： 12/31/25
+package com.che2n3jigw.android.libs.opensubsonicapi.response.playlists
+
+import com.che2n3jigw.android.libs.opensubsonicapi.response.common.Child
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PlaylistWithSongs(
+    /**
+     * Id of the playlist
+     */
+    val id: String? = null,
+    /**
+     * Name of the playlist
+     */
+    val name: String? = null,
+    /**
+     * A comment
+     */
+    val comment: String? = null,
+    /**
+     * wner of the playlist
+     */
+    val owner: String? = null,
+    /**
+     * Is the playlist public
+     */
+    val public: Boolean? = null,
+    /**
+     * number of songs
+     */
+    val songCount: Int? = null,
+    /**
+     * Playlist duration in seconds
+     */
+    val duration: Int? = null,
+    /**
+     * Creation date
+     */
+    val created: String? = null,
+    /**
+     * Last changed date
+     */
+    val changed: String? = null,
+    /**
+     * A cover Art Id
+     */
+    val coverArt: String? = null,
+    /**
+     * A list of allowed usernames
+     */
+    val allowedUser: List<String?>? = null,
+    /**
+     * The list of songs
+     */
+    val entry: List<Child?>? = null
+)
